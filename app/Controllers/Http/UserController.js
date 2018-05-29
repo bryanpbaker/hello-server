79520df6da8f4f885ffe71d30361de097c941bcc
Bryan Baker <bryanpbaker@gmail.com>
2018-05-29 09:17:23 -0500

    added prettier and eslint

---
+++ app/Controllers/Http/UserController.js
@@ -0,0 +1,27 @@
+'use strict'
+
+class UserController {
+  async index () {
+  }
+
+  async create () {
+  }
+
+  async store ({request, response}) {
+
+  }
+
+  async show () {
+  }
+
+  async edit () {
+  }
+
+  async update () {
+  }
+
+  async destroy () {
+  }
+}
+
+module.exports = UserController
