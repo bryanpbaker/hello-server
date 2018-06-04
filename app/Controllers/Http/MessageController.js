06ef0a7a7dec1fafc7ae9f94faeade2126d96a93
Bryan Baker <bryanpbaker@gmail.com>
2018-06-04 12:54:43 -0500

    message controller .store() WIP

--- app/Controllers/Http/MessageController.js
+++ app/Controllers/Http/MessageController.js
@@ -9,39 +9,18 @@ class MessageController {
   async create() {}
 
   async store({ request, auth }) {
-    const { recipientId, message } = request.body;
-    const userId = auth.user.id;
+    if (!request.conversation) {
+      console.log('no way man');
+    } else {
+      console.log('alright here you go');
+    }
 
-    // // find the recipient specified in the request
-    // const recipient = await User.find(recipientId);
-    // // find the user who created the message
-    // const user = await User.find(userId);
-    // // grab the conversations belonging to the recipient
-    // const recipientConversations = await recipient.conversations().fetch();
-    // // create an array of the recipients conversation ids
-    // const recipientConversationIds = recipientConversations.rows.map(
-    //   convo => convo.$attributes.id
-    // );
-    // // check if the user has any conversations with an id matching
-    // // any one of the recipient's conversation ids
-    // const userConversations = await user
-    //   .conversations()
-    //   .whereInPivot('conversation_id', recipientConversationIds)
-    //   .fetch();
-
-    // // if a conversation exists between the two users, fetch that conversation
-    // if (userConversations.rows.length > 0) {
-    //   return 'yes!';
-    // }
-    // return 'no!';
     // instantiate new Conversation if one does not yet exist
     // between the two users
     // const conversation = new Conversation();
     // const user1 = await User.find(userId);
     // const user2 = await User.find(recipientId);
-
     // await conversation.users().saveMany([user1, user2]);
-
     // return 'Success!';
   }
 
--- app/Middleware/FindConversation.js
+++ app/Middleware/FindConversation.js
@@ -5,22 +5,29 @@ class FindConversation {
   async handle({ request, auth }, next) {
     const userId = auth.user.id;
     const { recipientId } = request.body;
-    const user = User.find(userId);
 
+    // query the conversation_user pivot table for the user's conversations
     const userConversations = await Database.table('conversation_user').where(
       'user_id',
       userId
     );
-
+    // create an array of conversation_id from the user's conversations
     const userConversationIds = userConversations.map(
       conversation => conversation.conversation_id
     );
-
+    // query the conversation_user pivot table to see if the recipient
+    // shares a common conversation with the user
     const conversation = await Database.table('conversation_user')
       .whereIn('conversation_id', userConversationIds)
       .where('user_id', recipientId);
 
-    console.log(conversation);
+    // set request.conversation equal to the conversation
+    // if it exists, or null if not
+    if (conversation.length > 0) {
+      request.conversation = conversation;
+    } else {
+      request.conversation = null;
+    }
     // call next to advance the request
     await next();
   }
