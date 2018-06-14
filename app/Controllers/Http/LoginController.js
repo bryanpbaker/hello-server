8e4cbcb4ca17e3633fbe0d81d0a1a4cfa80aea09
Bryan Baker <bryanpbaker@gmail.com>
2018-06-14 14:00:53 -0500

    WIP facebook auth

--- app/Controllers/Http/LoginController.js
+++ app/Controllers/Http/LoginController.js
@@ -2,11 +2,13 @@ const User = use('App/Models/User');
 
 class LoginController {
   async verifyFbToken({ request, response, ally, auth }) {
-    const body = request.all();
+    const { accessToken } = request.all();
+    const user = await ally.driver('facebook').getUserByToken(accessToken);
+
+    console.log(user);
 
     return {
-      success: true,
-      message: 'test'
+      success: true
     };
   }
 }
